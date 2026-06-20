7cb59e8a64dff3284f11272004d5e7c3fbd3c5ad
Kavya N <nkavya649@gmail.com>
2026-06-20 13:55:12 +0530

    fixing 3

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,48 +1,30 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-const multer = require('multer');
-const {CloudinaryStorage} = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary');
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => ({
+        folder: "pdf_folder",
+        resource_type: "raw",
+        public_id: Date.now() + "-" + file.originalname.split(".")[0],
+    }),
+});
 
-let storage;
-
-if(process.env.NODE_ENV === "production"){
-    storage = new CloudinaryStorage({
-        cloudinary,
-        params:{
-            folder:"pdf_folder",
-            resource_type:"raw",
-        }
-    })
-}else{
-    storage = multer.diskStorage({
-        destination:(req,file,cb)=>{
-            cb(null,"uploads/");
-        },
-
-        filename:(req,file,cb)=>{
-            const uniqueName = Date.now() + "-" + file.originalname;
-            cb(null,uniqueName);
-        }
-    })
-}
-
-
-const fileFilter = (req,file,cb)=>{
-    if(file.mimetype === "application/pdf"){
-        cb(null,true);
-    }else{
-        cb(new Error("only pdf files are allowed"),false);
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+        cb(null, true);
+    } else {
+        cb(new Error("Only PDF files are allowed"), false);
     }
 };
 
 const upload = multer({
-    storage:storage,
-    fileFilter:fileFilter,
-    limits:{
-        fileSize:5 * 1024 * 1024
-    }
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB
+    },
 });
 
 module.exports = upload;
-
-
